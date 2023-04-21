2637cd7a12120ff13b31afa02df79e15ddc535b5
Kim Seonmin <w.hand715@gmail.com>
2023-04-21 09:55:57 +0900

    Entity 설계도 기반 Entity 클래스 작성

---
+++ src/main/java/com/sparta/springlv2/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.sparta.springlv2.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne // 연관관계의 주인
+    @JoinColumn(name = "memo_id")
+    private Memo memo;
+
+    public void setMemo(Memo memo) {
+        this.memo = memo;
+        memo.getComments().add(this);
+    }
+}
